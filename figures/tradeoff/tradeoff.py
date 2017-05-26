76e6f724e965aece762fac5c4d3484c0489f9a50
Jed Brown <jed@jedbrown.org>
2017-05-26 08:11:49 -0600

    Add draft 2017 MUN tutorial and figures

--- figures/tradeoff/tradeoff.py
+++ figures/tradeoff/tradeoff.py
@@ -32,6 +32,38 @@ def mkplot(title, cost, filename, dim):
    pyplot.savefig(filename)
    pyplot.clf()
 
-mkplot('Accuracy-Cost Tradeoff for Parabolic equation, FMG', cost_fmg, 'AccuracyVsCostFMG3D.pdf', dim=3)
-mkplot('Accuracy-Cost Tradeoff for Parabolic equation, LU', cost_lu, 'AccuracyVsCostLU2D.pdf', dim=2)
-mkplot('Accuracy-Cost Tradeoff for Parabolic equation, LU', cost_lu, 'AccuracyVsCostLU3D.pdf', dim=3)
+# mkplot('Accuracy-Cost Tradeoff for Parabolic equation, FMG', cost_fmg, 'AccuracyVsCostFMG3D.pdf', dim=3)
+# mkplot('Accuracy-Cost Tradeoff for Parabolic equation, LU', cost_lu, 'AccuracyVsCostLU2D.pdf', dim=2)
+# mkplot('Accuracy-Cost Tradeoff for Parabolic equation, LU', cost_lu, 'AccuracyVsCostLU3D.pdf', dim=3)
+
+def stoch_cost_mcmc(dx, implicit, dim, sdim, cost):
+    e = error(dt,dx,implicit)
+    n = e ** (-2)
+    return n * cost(dx, implicit)
+def stoch_cost_pce(dt, dx, implicit, dim, sdim, cost):
+    e = error(dt,dx,implicit)
+    n = numpy.log(1/e) ** sdim
+    return n * cost(dt, dx, implicit)
+def error_stoch_mcmc(M,dx,sdim):
+    return dx**2 + M ** (-0.5)
+def error_stoch_pce(M,dx,sdim):
+    return dx**2 + 10*numpy.exp(- M ** (1/sdim))
+def mkplot_stoch(title, error, filename, dim, sdim):
+    M = numpy.logspace(0,5,100)
+    lines = []
+    for dx in [.05, .1, .2, .4, .8]:
+        lines += pyplot.loglog(M*cost_fmg(dx,dx,implicit=True,dim=dim), error_stoch_mcmc(M,dx,sdim), '-')
+        lines += pyplot.loglog(M*cost_fmg(dx,dx,implicit=True,dim=dim), error_stoch_pce(M,dx,sdim), '.')
+    pyplot.title(title + ', %d parameters' % (sdim,))
+    pyplot.ylabel('Spatial+Stochastic Error')
+    pyplot.xlabel('Cost')
+    pyplot.ylim(0.7*min(error_stoch_mcmc(M[-1],.05,sdim),error_stoch_pce(M[-1],.05,sdim)), 8)
+    pyplot.legend(lines[-2:], ('MCMC', 'Spectral PCE'), loc='lower left')
+    pyplot.savefig(filename)
+    pyplot.clf()
+
+mkplot_stoch('Accuracy-Cost Tradeoff for MCMC vs PCE', error_stoch_mcmc, 'AccuracyVsCostS3.pdf', dim=3, sdim=3)
+mkplot_stoch('Accuracy-Cost Tradeoff for MCMC vs PCE', error_stoch_mcmc, 'AccuracyVsCostS4.pdf', dim=3, sdim=4)
+mkplot_stoch('Accuracy-Cost Tradeoff for MCMC vs PCE', error_stoch_mcmc, 'AccuracyVsCostS5.pdf', dim=3, sdim=5)
+mkplot_stoch('Accuracy-Cost Tradeoff for MCMC vs PCE', error_stoch_mcmc, 'AccuracyVsCostS6.pdf', dim=3, sdim=6)
+mkplot_stoch('Accuracy-Cost Tradeoff for MCMC vs PCE', error_stoch_mcmc, 'AccuracyVsCostS8.pdf', dim=3, sdim=8)
